d9d6604908bf4c0c3da1e8442ebc006bb69696a7
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-09-02 19:38:59 +0530

    Create leetCode_3875.cpp

---
+++ DAY_6/leetCode_3875.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<vector>
+using namespace std;
+
+int main(){
+ system("cls");
+    
+    return 0;
+}
+
+class Solution {
+public:
+    bool uniformArray(vector<int>& nums1) {
+        return true;
+        
+    }
+};
